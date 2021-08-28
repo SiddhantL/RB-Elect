5b89d61a158a8edd6442fbfe151427ab9451792b
Siddhant Lad <32464449+SiddhantL@users.noreply.github.com>
2021-08-28 20:25:50 +0530

    Add files via upload

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestPostgres.Interfaces;
+using TestPostgres.Models;
+
+namespace TestPostgres.Controllers
+{
+    [Route("api/[controller]")]
+    public class EventsController : ControllerBase
+    {
+        private readonly IDataAccessProvider _dataAccessProvider;
+
+        public EventsController(IDataAccessProvider dataAccessProvider)
+        {
+            _dataAccessProvider = dataAccessProvider;
+        }
+
+        [HttpGet]
+        public IEnumerable<Events> Get()
+        {
+            return _dataAccessProvider.GetEventRecords();
+        }
+
+        /*   [HttpPost]
+           public IActionResult Create([FromBody] Patient patient)
+           {
+               if (ModelState.IsValid)
+               {
+                   Guid obj = Guid.NewGuid();
+                   patient.id = obj.ToString();
+                   _dataAccessProvider.AddPatientRecord(patient);
+                   return Ok();
+               }
+               return BadRequest();
+           }
+
+           [HttpGet("{id}")]
+           public Patient Details(string id)
+           {
+               return _dataAccessProvider.GetPatientSingleRecord(id);
+           }
+
+           [HttpPut]
+           public IActionResult Edit([FromBody] Patient patient)
+           {
+               if (ModelState.IsValid)
+               {
+                   _dataAccessProvider.UpdatePatientRecord(patient);
+                   return Ok();
+               }
+               return BadRequest();
+           }
+
+           [HttpDelete("{id}")]
+           public IActionResult DeleteConfirmed(string id)
+           {
+               var data = _dataAccessProvider.GetPatientSingleRecord(id);
+               if (data == null)
+               {
+                   return NotFound();
+               }
+               _dataAccessProvider.DeletePatientRecord(id);
+               return Ok();
+           }*/
+    }
+}
+
